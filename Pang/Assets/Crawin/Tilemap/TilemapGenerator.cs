a476b6d17f5146141abf4ecd29847a515e90103e
Lee Seung Hak <99tmdgkr@gmail.com>
2025-08-23 23:59:23 +0900

    타일 맵 생성 타일맵의 데이터를 json파일로 할당하여 추가

---
+++ Pang/Assets/Crawin/Tilemap/TilemapGenerator.cs
@@ -0,0 +1,78 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+[System.Serializable]
+public class MapJson
+{
+    public int rows;
+    public int cols;
+    public int[] data;
+}
+
+[ExecuteInEditMode]
+public class TilemapGenerator: MonoBehaviour
+{
+    public TextAsset jsonFile;
+    public GameObject tilePrefab;
+    public GameObject wallPrefab;
+    private bool regenerateMap = true;
+    private GameObject tileParent;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        if(jsonFile != null && regenerateMap)
+        {
+            GenerateMap();
+        }    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (jsonFile != null && regenerateMap)
+        {
+            GenerateMap();
+        }
+    }
+
+    private void OnValidate()
+    {
+        Debug.Log("Validate");
+        regenerateMap = true;
+    }
+
+    void GenerateMap()
+    {
+        if (tileParent != null)
+        {
+            DestroyImmediate(tileParent);
+        }
+        else
+        {
+            GameObject old = GameObject.Find("TileParent");
+            if (old != null)
+            {
+                DestroyImmediate(old);
+            }
+        }
+        tileParent = new GameObject("TileParent");
+        tileParent.transform.parent = transform;
+
+        MapJson map = JsonUtility.FromJson<MapJson>(jsonFile.text);
+        for (int z = 0; z < map.rows; ++z)
+        {
+            for (int x = 0; x < map.cols; ++x)
+            {
+                int value = map.data[z * map.cols + x];
+                Vector3 pos = new Vector3(x, 0, -z);
+
+                if (value == 1)
+                    Instantiate(wallPrefab, pos, wallPrefab.transform.rotation, tileParent.transform);
+                else
+                    Instantiate(tilePrefab, pos, tilePrefab.transform.rotation, tileParent.transform);
+            }
+        }
+        regenerateMap = false;
+    }
+}
