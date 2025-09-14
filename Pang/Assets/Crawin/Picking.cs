42a3b0b9b7e0127eae998dc59f8f43a4d1c91c17
Lee Seung Hak <99tmdgkr@gmail.com>
2025-09-14 23:14:21 +0900

    TilemapGenerator에서 타일맵을 제작할 때, Dictionary<vector2Int, Gameobejct>를 추가 생성하여 해당 위치에 존재하는 오브젝트를 저장해둠 -> 메모리는 낭비 되지만, 충돌 검사를 할 필요가 없기에 속도가 빠름 제작된 딕셔너리를 picking에서 레퍼런스로 받아와서 벽(오브젝트) 배치 시 딕셔너리와 mapdata에 할당, 제거 시 딕셔너리와 mapdata에서 제거

--- Pang/Assets/Crawin/Picking.cs
+++ Pang/Assets/Crawin/Picking.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,9 +8,9 @@ public class Picking : MonoBehaviour
     TilemapGenerator gen;
     private MapJson map;
     public GameObject buildingPrefab;
-    public Material[] mat;
     public GameObject previewPrefab;
     private GameObject previewInstance;
+    private Dictionary<Vector2Int, GameObject> buildings;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -28,6 +29,7 @@ public class Picking : MonoBehaviour
         else
         {
             map = gen.mapRef;
+            buildings = gen.buildingsRef;
         }
         if (map == null)
         {
@@ -59,9 +61,11 @@ public class Picking : MonoBehaviour
             // 월드 좌표 → 타일 인덱스
             int tileX = Mathf.FloorToInt(worldPos.x + 0.5f);
             int tileZ = Mathf.FloorToInt(worldPos.z + 0.5f);
+            Vector2Int v2i = new Vector2Int(tileZ, tileX);
+
             //Debug.Log($"마우스로{tileX},{tileZ}를 클릭");
             // 배열 범위 체크
-            Vector3 placePos = new Vector3(tileX, 0, tileZ);
+            Vector3 placePos = new Vector3(tileX, 0.5f, tileZ);
             if (tileX >= 0 && tileX < map.rows && tileZ >= 0 && tileZ < map.cols)
             {
                 if (map.data[tileZ * map.cols + tileX] == 0) // 바닥이면
@@ -78,20 +82,22 @@ public class Picking : MonoBehaviour
                 }
 
                 if (Input.GetMouseButtonDown(0)) {      // 좌클릭 했을 때
-                    Debug.Log($"{placePos}를 클릭했담");
+                    //Debug.Log($"{placePos}를 클릭했담");
                     if (map.data[tileZ * map.cols + tileX] == 0) // 바닥이면
                     {
-                        Instantiate(buildingPrefab, placePos, Quaternion.identity);
+                        buildings[v2i] = Instantiate(buildingPrefab, placePos, Quaternion.identity);
                         Debug.Log($"벽 생성: ({tileX}, {tileZ})");
                         map.data[tileZ * map.cols + tileX] = 1;
                     }
                     else if (map.data[tileZ * map.cols + tileX] == 1)
                     {
-                        Debug.Log("벽임당");
+                        Debug.Log($"벽 제거: ({tileX}, {tileZ})");
+                        Destroy(buildings[v2i]);
+                        buildings.Remove(v2i);
                         //Vector3 placePos = new Vector3(tileX, 0, tileZ);
                         //Instantiate(buildingPrefab, placePos, Quaternion.identity);
                         //Debug.Log($"벽 제거: ({tileX}, {tileZ})");
-                        //map.data[tileZ * map.cols + tileX] = 0;
+                        map.data[tileZ * map.cols + tileX] = 0;
                     }
                 }
             }
--- Pang/Assets/Crawin/Tilemap/TilemapGenerator.cs
+++ Pang/Assets/Crawin/Tilemap/TilemapGenerator.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -21,6 +22,8 @@ public class TilemapGenerator: MonoBehaviour
     private MapJson map;
     public ref MapJson mapRef => ref map;
 
+    private Dictionary<Vector2Int, GameObject> buildings;
+    public ref Dictionary<Vector2Int,GameObject> buildingsRef => ref buildings;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -31,6 +34,7 @@ public class TilemapGenerator: MonoBehaviour
     {
         if (jsonFile != null && regenerateMap)
         {
+            Debug.Log("Generator Awake");
             GenerateMap();
         }
     }
@@ -38,6 +42,8 @@ public class TilemapGenerator: MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log("update");
+        //printMap();
         if (jsonFile != null && regenerateMap)
         {
             GenerateMap();
@@ -68,17 +74,29 @@ public class TilemapGenerator: MonoBehaviour
         tileParent.transform.parent = transform;
 
         map = JsonUtility.FromJson<MapJson>(jsonFile.text);
+        buildings = new Dictionary<Vector2Int, GameObject>();
+        //bool even = false;
+        //GameObject tile = tilePrefab;
+        Vector2Int v2i = new Vector2Int();
         for (int z = 0; z < map.rows; ++z)
         {
             for (int x = 0; x < map.cols; ++x)
             {
                 int value = map.data[z * map.cols + x];
+                v2i.x = z;
+                v2i.y = x;
+                //if (even)
+                //    tile.GetComponent<Renderer>().material.color = new Color(1, 1, 1, 1f);
+                //else
+                //    tile.GetComponent<Renderer>().material.color = new Color(0, 0, 0, 1f);
                 Vector3 pos = new Vector3(x, 0, z);
-
-                if (value == 1)
-                    Instantiate(wallPrefab, pos, wallPrefab.transform.rotation, tileParent.transform);
-                else
-                    Instantiate(tilePrefab, pos, tilePrefab.transform.rotation, tileParent.transform);
+                //일단 타일을 깔아
+                Instantiate(tilePrefab, pos, tilePrefab.transform.rotation, tileParent.transform);
+                if (value == 1) // 벽이면 타일 위에 벽을 설치해
+                {
+                    pos.y = 0.5f;
+                    buildings[v2i] = Instantiate(wallPrefab, pos, wallPrefab.transform.rotation, tileParent.transform);
+                }
             }
         }
         regenerateMap = false;
