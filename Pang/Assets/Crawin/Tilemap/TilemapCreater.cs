50a51c5a8ba534a10dfac54ec4f4b68a0741f1e2
Lee Seung Hak <99tmdgkr@gmail.com>
2025-09-18 19:40:50 +0900

    TilemapCreater로 가로 세로 갯수를 정해주면 json파일 생성 그러나 100x100을 하니 너무 많은 오브젝트로 인한 프레임 드랍 -> 인스턴싱 사용 필요 -> 마우스 피킹에서 건물 제거 방법도 수정 필요

---
+++ Pang/Assets/Crawin/Tilemap/TilemapCreater.cs
@@ -0,0 +1,76 @@
+using NUnit.Framework;
+using System;
+using System.IO;
+using UnityEngine;
+
+public class TilemapCreater : MonoBehaviour
+{
+    public enum MapType
+    {
+        Empty,
+        Custom,
+        Random
+    }
+    public MapType type;
+    public int rows;
+    public int cols;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateMap()
+    {
+        MapJson mapJson = new MapJson();
+        string json;
+        string outputPath;
+        switch (type)
+        {
+            case (MapType.Empty):
+                mapJson.rows = rows;
+                mapJson.cols = cols;
+                mapJson.data = new int[rows * cols];
+                for(int y = 0; y < rows; ++y)
+                {
+                    for (int x = 0; x < cols; ++x)
+                    {
+                        int index = y * cols + x;
+                        mapJson.data[index] = 0;
+                    }
+                }
+
+                json = JsonUtility.ToJson(mapJson);
+                outputPath = Path.Combine(Application.dataPath, "mapdata.json");
+                File.WriteAllText(outputPath, json);
+                break;
+            case (MapType.Custom):
+
+                break;
+            case (MapType.Random):
+                mapJson.rows = rows;
+                mapJson.cols = cols;
+                mapJson.data = new int[rows * cols];
+                for (int y = 0; y < rows; ++y)
+                {
+                    for (int x = 0; x < cols; ++x)
+                    {
+                        int index = y * cols + x;
+                        mapJson.data[index] = UnityEngine.Random.Range(0, 2);
+                    }
+                }
+
+                json = JsonUtility.ToJson(mapJson);
+                outputPath = Path.Combine(Application.dataPath, "mapdata.json");
+                File.WriteAllText(outputPath, json);
+                break;
+        }
+    }
+}
