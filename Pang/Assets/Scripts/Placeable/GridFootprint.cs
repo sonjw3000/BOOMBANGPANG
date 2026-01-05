67c1948718a5c2307feec60ee6190b6f56684538
Son Jungwon <sonjw3000@gmail.com>
2026-01-05 18:45:04 +0900

    피봇 다시 추가

--- Pang/Assets/Scripts/Placeable/GridFootprint.cs
+++ Pang/Assets/Scripts/Placeable/GridFootprint.cs
@@ -16,6 +16,8 @@ public sealed class GridFootprint : ScriptableObject
 	[Min(1)] public int width = 1;
 	[Min(1)] public int height = 1;
 
+	[SerializeField] private Vector2Int pivot = new Vector2Int(0, 0);
+
 	[SerializeField] private FootprintCell[] footprintCells;
 
 
