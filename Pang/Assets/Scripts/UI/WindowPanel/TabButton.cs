8b831835dce9f23d92b979cf4d5e4bfc89dc9136
Son Jungwon <sonjw3000@gmail.com>
2026-05-08 14:39:58 +0900

    인덴트 수정

--- Pang/Assets/Scripts/UI/WindowPanel/TabButton.cs
+++ Pang/Assets/Scripts/UI/WindowPanel/TabButton.cs
@@ -5,32 +5,32 @@ using System;
 
 namespace Assets.Scripts.UI
 {
-    public class TabButton : MonoBehaviour
-    {
-        [SerializeField] private TextMeshProUGUI labelText;
-        [SerializeField] private Image background;
-        [SerializeField] private Button button;
+	public class TabButton : MonoBehaviour
+	{
+		[SerializeField] private TextMeshProUGUI labelText;
+		[SerializeField] private Image background;
+		[SerializeField] private Button button;
 
-        [Header("Settings")]
-        public Color selectedColor = Color.white;
-        public Color unselectedColor = Color.grey;
+		[Header("Settings")]
+		public Color selectedColor = Color.white;
+		public Color unselectedColor = Color.grey;
 
-        private Action<int> onClick;
-        private int tabIndex;
+		private Action<int> onClick;
+		private int tabIndex;
 
-        public void Init(int index, string label, Action<int> callback)
-        {
-            tabIndex = index;
-            labelText.text = label;
-            onClick = callback;
-            button.onClick.AddListener(() => onClick?.Invoke(tabIndex));
-        }
+		public void Init(int index, string label, Action<int> callback)
+		{
+			tabIndex = index;
+			labelText.text = label;
+			onClick = callback;
+			button.onClick.AddListener(() => onClick?.Invoke(tabIndex));
+		}
 
-        public void SetSelected(bool selected)
-        {
-            background.color = selected ? selectedColor : unselectedColor;
-            // Optionally change text color or style
-            labelText.fontWeight = selected ? FontWeight.Bold : FontWeight.Regular;
-        }
-    }
+		public void SetSelected(bool selected)
+		{
+			background.color = selected ? selectedColor : unselectedColor;
+			// Optionally change text color or style
+			labelText.fontWeight = selected ? FontWeight.Bold : FontWeight.Regular;
+		}
+	}
 }
--- Pang/Assets/Scripts/UI/WindowPanel/Worker/WorkerWindow.cs
+++ Pang/Assets/Scripts/UI/WindowPanel/Worker/WorkerWindow.cs
@@ -5,143 +5,143 @@ using System.Linq;
 
 namespace Assets.Scripts.UI
 {
-    public class WorkerWindow : MonoBehaviour
-    {
-        public enum TabType
-        {
-            Unassigned,
-            All,
-            Unloading,
-            Storing,
-            Picking,
-            Packing,
-            Loading,
-            Water
-        }
+	public class WorkerWindow : MonoBehaviour
+	{
+		public enum TabType
+		{
+			Unassigned,
+			All,
+			Unloading,
+			Storing,
+			Picking,
+			Packing,
+			Loading,
+			Water
+		}
 
-        [SerializeField] private UIWindow window;
-        [SerializeField] private WorkerItemView itemPrefab;
-        [SerializeField] private Transform listRoot;
+		[SerializeField] private UIWindow window;
+		[SerializeField] private WorkerItemView itemPrefab;
+		[SerializeField] private Transform listRoot;
 
-        [Header("Window MetaData")]
-        [SerializeField] private string title = "Worker Management";
-        [SerializeField] private Sprite icon;
+		[Header("Window MetaData")]
+		[SerializeField] private string title = "Worker Management";
+		[SerializeField] private Sprite icon;
 
-        private WorkerManager workerMgr => GameContext.Instance.WorkerMgr;
-        private TabType currentTab = TabType.All;
-        private List<WorkerItemView> activeItems = new List<WorkerItemView>();
-        private bool tabsInitialized = false;
+		private WorkerManager workerMgr => GameContext.Instance.WorkerMgr;
+		private TabType currentTab = TabType.All;
+		private List<WorkerItemView> activeItems = new List<WorkerItemView>();
+		private bool tabsInitialized = false;
 
-        private void Awake()
-        {
-            if (window == null) window = GetComponentInChildren<UIWindow>(true);
-            
-            window.SetTitle(title);
-            window.SetIcon(icon);
-            SetupTabs();
-            gameObject.SetActive(false);
-        }
+		private void Awake()
+		{
+			if (window == null) window = GetComponentInChildren<UIWindow>(true);
 
-        private void SetupTabs()
-        {
-            if (tabsInitialized) return;
-            window.ClearTabs();
-            
-            // Order as requested: Unassigned, All, TaskTypes
-            window.AddTab("Unassigned", SetTab);
-            window.AddTab("All", SetTab);
-            window.AddTab("Unloading", SetTab);
-            window.AddTab("Storing", SetTab);
-            window.AddTab("Picking", SetTab);
-            window.AddTab("Packing", SetTab);
-            window.AddTab("Loading", SetTab);
-            window.AddTab("Water", SetTab);
-            
-            window.UpdateTabVisuals((int)currentTab);
-            tabsInitialized = true;
-        }
+			window.SetTitle(title);
+			window.SetIcon(icon);
+			SetupTabs();
+			gameObject.SetActive(false);
+		}
 
-        private void OnEnable()
-        {
-            SetupTabs();
-            RefreshList();
-        }
+		private void SetupTabs()
+		{
+			if (tabsInitialized) return;
+			window.ClearTabs();
 
-        private void Update()
-        {
-            if (Time.frameCount % 30 == 0)
-            {
-                RefreshList();
-            }
-        }
+			// Order as requested: Unassigned, All, TaskTypes
+			window.AddTab("Unassigned", SetTab);
+			window.AddTab("All", SetTab);
+			window.AddTab("Unloading", SetTab);
+			window.AddTab("Storing", SetTab);
+			window.AddTab("Picking", SetTab);
+			window.AddTab("Packing", SetTab);
+			window.AddTab("Loading", SetTab);
+			window.AddTab("Water", SetTab);
 
-        public void Open()
-        {
-            gameObject.SetActive(true);
-            window.Open();
-        }
+			window.UpdateTabVisuals((int)currentTab);
+			tabsInitialized = true;
+		}
 
-        public void Close()
-        {
-            window.Close();
-            gameObject.SetActive(false);
-        }
+		private void OnEnable()
+		{
+			SetupTabs();
+			RefreshList();
+		}
 
-        public void SetTab(int tabIndex)
-        {
-            currentTab = (TabType)tabIndex;
-            window.UpdateTabVisuals(tabIndex);
-            RefreshList();
-        }
+		private void Update()
+		{
+			if (Time.frameCount % 30 == 0)
+			{
+				RefreshList();
+			}
+		}
 
-        private void RefreshList()
-        {
-            if (workerMgr == null) return;
+		public void Open()
+		{
+			gameObject.SetActive(true);
+			window.Open();
+		}
 
-            var workers = workerMgr.Workers;
-            var filteredWorkers = workers.Where(ShouldShowInTab).ToList();
+		public void Close()
+		{
+			window.Close();
+			gameObject.SetActive(false);
+		}
 
-            // Simple management: if counts differ, clear and rebuild. 
-            // Better would be updating existing items.
-            if (activeItems.Count != filteredWorkers.Count)
-            {
-                foreach (var item in activeItems)
-                {
-                    if (item != null) Destroy(item.gameObject);
-                }
-                activeItems.Clear();
+		public void SetTab(int tabIndex)
+		{
+			currentTab = (TabType)tabIndex;
+			window.UpdateTabVisuals(tabIndex);
+			RefreshList();
+		}
 
-                foreach (var worker in filteredWorkers)
-                {
-                    var item = Instantiate(itemPrefab, listRoot);
-                    item.Setup(worker);
-                    activeItems.Add(item);
-                }
-            }
-            else
-            {
-                for (int i = 0; i < filteredWorkers.Count; i++)
-                {
-                    activeItems[i].Setup(filteredWorkers[i]);
-                }
-            }
-        }
+		private void RefreshList()
+		{
+			if (workerMgr == null) return;
 
-        private bool ShouldShowInTab(AIWorker worker)
-        {
-            if (currentTab == TabType.All) return true;
-            if (currentTab == TabType.Unassigned) return worker.TaskType == WorkerTask.TaskType.Undefined;
-            
-            switch (currentTab)
-            {
-                case TabType.Unloading: return worker.TaskType == WorkerTask.TaskType.Unloading;
-                case TabType.Storing: return worker.TaskType == WorkerTask.TaskType.Storing;
-                case TabType.Picking: return worker.TaskType == WorkerTask.TaskType.Picking;
-                case TabType.Packing: return worker.TaskType == WorkerTask.TaskType.Packing;
-                case TabType.Loading: return worker.TaskType == WorkerTask.TaskType.Loading;
-                case TabType.Water: return worker.TaskType == WorkerTask.TaskType.Water;
-            }
-            return false;
-        }
-    }
+			var workers = workerMgr.Workers;
+			var filteredWorkers = workers.Where(ShouldShowInTab).ToList();
+
+			// Simple management: if counts differ, clear and rebuild. 
+			// Better would be updating existing items.
+			if (activeItems.Count != filteredWorkers.Count)
+			{
+				foreach (var item in activeItems)
+				{
+					if (item != null) Destroy(item.gameObject);
+				}
+				activeItems.Clear();
+
+				foreach (var worker in filteredWorkers)
+				{
+					var item = Instantiate(itemPrefab, listRoot);
+					item.Setup(worker);
+					activeItems.Add(item);
+				}
+			}
+			else
+			{
+				for (int i = 0; i < filteredWorkers.Count; i++)
+				{
+					activeItems[i].Setup(filteredWorkers[i]);
+				}
+			}
+		}
+
+		private bool ShouldShowInTab(AIWorker worker)
+		{
+			if (currentTab == TabType.All) return true;
+			if (currentTab == TabType.Unassigned) return worker.TaskType == WorkerTask.TaskType.Undefined;
+
+			switch (currentTab)
+			{
+				case TabType.Unloading: return worker.TaskType == WorkerTask.TaskType.Unloading;
+				case TabType.Storing: return worker.TaskType == WorkerTask.TaskType.Storing;
+				case TabType.Picking: return worker.TaskType == WorkerTask.TaskType.Picking;
+				case TabType.Packing: return worker.TaskType == WorkerTask.TaskType.Packing;
+				case TabType.Loading: return worker.TaskType == WorkerTask.TaskType.Loading;
+				case TabType.Water: return worker.TaskType == WorkerTask.TaskType.Water;
+			}
+			return false;
+		}
+	}
 }
